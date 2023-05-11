587bfa0e9c2d3e65731a8cc51fe08f42f8186321
mateo <89844584+mateo1665@users.noreply.github.com>
2023-05-11 11:43:54 +0200

    Delete speelhal.py

--- speelhal.py
+++
@@ -1,9 +0,0 @@
-persoon = 4
-minuten = 5
-toegangsticket = 745
-gameset = 37
-
-input(f'een ticket voor 1 persoon kost {toegangsticket} en als je op de gameset wilt kost het {gameset} cent voor {minuten} minuten')
-input('jullie zijn met 4 personen en willen 45 minuten in de gameset')
-
-print('in totaal is het',persoon * toegangsticket + gameset + minuten * 9,'cent')
