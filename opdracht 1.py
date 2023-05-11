eb5b445051014c4458064084598eca2c31d2fee9
mateo <89844584+mateo1665@users.noreply.github.com>
2023-05-11 11:44:28 +0200

    Delete opdracht 1.py

--- opdracht 1.py
+++
@@ -1,8 +0,0 @@
-def bereken_poten(giraffen,struisvogels,zebras):
-    giraffen = 4
-    struisvogels = 2
-    zebras = 4
-    totaal = giraffen + struisvogels + zebras
-    return totaal
-    
-print(bereken_poten(5,2,3))
