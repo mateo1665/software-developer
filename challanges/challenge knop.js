4a0dd77344ef61ad52c9c386442ab8fa26a200cc
mateo <89844584+mateo1665@users.noreply.github.com>
2023-05-11 11:50:00 +0200

    Add files via upload

---
+++ challanges/challenge knop.js
@@ -0,0 +1,8 @@
+let button = document.createElement("button");
+button.innerHTML = "hallo";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+});
